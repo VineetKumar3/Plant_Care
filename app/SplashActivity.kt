db738391de5ba07e8c0a919ddf6764e3ecabbf6a
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2026-03-31 09:08:09 +0530

    Add SplashActivity with delay to HomeActivity

---
+++ app/SplashActivity.kt
@@ -0,0 +1,15 @@
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 2000) // 2 seconds delay
+    }
+}
